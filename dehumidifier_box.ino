010bf9f259bb98351cd0140dc23feb96291f30bd
Samrat Saha <aaniruddhaaa@gmail.com>
2020-08-28 15:54:56 +0530

    Add 1602 lcd display
    Add 1602 LCD display via i2c.

--- dehumidifier_box.ino
+++ dehumidifier_box.ino
@@ -13,14 +13,17 @@
  
 #include <Wire.h>
 #include "SparkFunHTU21D.h"
+#include <LiquidCrystal_I2C.h> // @todo Remove Later
 
 HTU21D myHumidity;
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // @todo Remove Later
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HTU21D Example!");
 
   myHumidity.begin();
+  lcd.begin(); // @todo Remove Later
+  lcd.backlight(); // @todo Remove Later
 }
 
 void loop() {
@@ -34,6 +37,13 @@ void loop() {
   Serial.print(humd, 1);
   Serial.print("%");
 
+  lcd.print(temp); 
+  lcd.print("°");
+
+  lcd.setCursor(1, 0);
+  lcd.print(humd); 
+  lcd.print("%");
+  
   Serial.println();
   delay(3000);
 }
