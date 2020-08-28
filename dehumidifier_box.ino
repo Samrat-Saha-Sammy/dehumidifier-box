77c364486435a467e17f248541ebd7a93d8489fe
Samrat Saha <aaniruddhaaa@gmail.com>
2020-08-28 16:38:15 +0530

    Add Fan Speed Graphics
    Add Fan Speed Graphics

--- dehumidifier_box.ino
+++ dehumidifier_box.ino
@@ -17,6 +17,27 @@
 
 HTU21D myHumidity;
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // @todo Remove Later
+byte fill[] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+}; // Fill Block
+byte empty[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00100,
+  B00100,
+  B00000,
+  B00000,
+  B00000
+}; // Unfilled Block
+int fanSpeed = 20;
 
 void setup() {
   Serial.begin(9600);
@@ -24,6 +45,8 @@ void setup() {
   myHumidity.begin();
   lcd.begin(); // @todo Remove Later
   lcd.backlight(); // @todo Remove Later
+  lcd.createChar(0, fill); 
+  lcd.createChar(1, empty); 
 }
 
 void loop() {
@@ -36,13 +59,27 @@ void loop() {
   Serial.print(" Humidity:");
   Serial.print(humd, 1);
   Serial.print("%");
+  
+  lcd.clear();
 
   lcd.print(temp); 
-  lcd.print("°");
+  lcd.print((char)223);
+  lcd.print("C");
 
-  lcd.setCursor(1, 0);
+  lcd.setCursor(10,0); // Col / Row
   lcd.print(humd); 
   lcd.print("%");
+
+  lcd.setCursor(0,1);
+  for(int i=0; i< 16; i++) {
+    
+    lcd.setCursor(i,1);
+    if(i < 5){
+      lcd.write(0);
+    } else {
+      lcd.write(1);
+    }
+  }
   
   Serial.println();
   delay(3000);
